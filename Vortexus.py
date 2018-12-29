a8d84073482638327e4be0c740c28c269d927a28
mayojar <43939932+majormayojar@users.noreply.github.com>
2018-12-29 16:59:25 -0500

    vortexus.py
    I added !debug test and !debug end

--- Vortexus.py
+++ Vortexus.py
@@ -1,32 +1,34 @@
 import discord
 from discord.ext import commands
 
-TOKEN='TOKEN HERE'
+TOKEN='NTI4Mzc1ODYwNTY3NDc0MTk2.Dwki5g.HzEEzjcvxv2I8IfuWjdlKrSJzks'
 
-client = commands.Bot(command_prefix="?")
+bot = commands.Bot(command_prefix = "!")
 
-@client.event
+@bot.event
 async def on_ready():
   print('Connected')
 
-@client.event 
+@bot.event 
 async def on_message(message):
-    if message.content == "ping":
-      await client.send_message(message.channel, "pong!")
-      await client.add_reaction(message, '\N{THUMBS UP SIGN}')
-    if message.content == "help":
+    if message.content == "!ping":
+      await bot.send_message(message.channel, "pong!")
+      await bot.add_reaction(message, '\N{THUMBS UP SIGN}')
+    if message.content == "!help":
       emb = (discord.Embed(description="Commands can be found [here](https://github.com/Infinixius/vortexus/wiki/Commands)", colour=0x3DF270))
       emb.set_author(name="Commands")
-      await client.send_message(message.channel, embed=emb)
-    if message.content == "about":
+      await bot.send_message(message.channel, embed=emb)
+    if message.content == "!about":
       emb = (discord.Embed(description="Vortexus is a bot maintained by [Infinixius](https://infinixius.co.nf), this is a port of Vortexus in discord.py by mayojar#7199. Run ?cmds for help. You are running version 1.4.0.0-beta", colour=0x3DF270))
       emb.set_author(name="About")
-      await client.send_message(message.channel, embed=emb)
-
+      await bot.send_message(message.channel, embed=emb)
+    if message.content == "!debug test":
+      print("Test")
+      await bot.add_reaction(message, '\N{THUMBS UP SIGN}')
+    if message.content == "!debug end":
+      await bot.close()
 
       
 
 
-
-client.run(TOKEN)
-
+bot.run(TOKEN)
