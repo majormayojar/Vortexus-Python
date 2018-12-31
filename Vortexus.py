d8c9c7b18ae6b151cc9e2332de1d7ced23ffdb1b
mayojar <43939932+majormayojar@users.noreply.github.com>
2018-12-31 13:51:33 -0500

    Update Vortexus.py

--- Vortexus.py
+++ Vortexus.py
@@ -20,7 +20,7 @@ async def on_message(message):
       await bot.send_message(message.channel, embed=emb)
     if message.content == "!about":
       emb = (discord.Embed(description="Vortexus is a bot maintained by [Infinixius](https://infinixius.co.nf), this is a port of Vortexus in discord.py by mayojar#7199. Run !help for help. You are running version 1.4.0.0-beta", colour=0x3DF270))
-      emb.set_author(name="About" icon_url='https://cdn.discordapp.com/avatars/257223769997705227/155bf4b3c722863069580acad968e48e.webp?size=2048')
+      emb.set_author(name="About" icon_url='https://cdn.discordapp.com/avatars/509499004959195156/293b9092c16ab87a3dbaaf632257ddac.webp?size=2048')
       await bot.send_message(message.channel, embed=emb)
     if message.content == "!debug test":
       print("Test")
