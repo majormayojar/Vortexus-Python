257bc694262291949ffc7cfd525081aa1fafcba6
mayojar <43939932+majormayojar@users.noreply.github.com>
2019-01-05 12:16:38 -0500

    Update Vortexus.py

--- Vortexus.py
+++ Vortexus.py
@@ -16,8 +16,8 @@ async def on_ready():
 
 @bot.event
 async def on_member_join():
-  rolee = discord.utils.get(server.roles, name='rolename here')
-  await bot.add_roles(member.server.roles, rolee)
+  rolee = discord.utils.get(server.roles, name='RoleNameHere')
+  await bot.add_roles(server.roles, rolee)
 
 @bot.command(pass_context=True)
 @commands.has_permissions(kick_members=True)
@@ -39,7 +39,7 @@ async def ban(ctx, member: discord.Member = None):
 async def mute(ctx, member: discord.Member=None):
     if not member:
         return await bot.say("Specify a member please")
-    role = discord.utils.get(ctx.message.server.roles, name="muted")
+    role = discord.utils.get(ctx.message.server.roles, name="muted") #make a role called muted  
     await bot.add_roles(member, role)
 
 @bot.command(pass_context=True)
@@ -49,6 +49,24 @@ async def unmute(ctx, member: discord.Member=None):
         return await bot.say("Specify a member please")
     role = discord.utils.get(ctx.message.server.roles, name="muted")
     await bot.remove_roles(member, role)
+
+@bot.command(pass_context=True)
+async def clear(ctx, amount=100):
+  channel = ctx.message.channel
+  messages = []
+  async for message in bot.logs_from(channel, limit=int(amount)):
+    messages.append(message)
+  await bot.delete_messages(messages)
+  await bot.say('Messages deleted')
+
+
+@bot.command()
+async def echo(*args):
+  output = ''
+  for word in args:
+    output += word
+    output += ' '
+  await bot.say(output)
 
 @bot.event 
 async def on_message(message):
@@ -67,27 +85,18 @@ async def on_message(message):
     if message.content == "!help":
       emb = (discord.Embed(description="Commands can be found [here](https://github.com/majormayojar/Vortexus-Python/wiki/Commands)", colour=0x3DF270))
       emb.set_author(name="Commands")
-      await bot.send_message(message.channel, embed=emb)
     if message.content == "!about":
       emb = (discord.Embed(description="Vortexus is a bot maintained by [Infinixius](https://infinixius.co.nf), this is a port of Vortexus in discord.py by [mayojar](mayojar.co.nf). Run ?cmds for help. You are running version 1.4.0.0-beta", colour=0x3DF270))
       emb.set_author(name="About")
       await bot.send_message(message.channel, embed=emb)
+    if message.content == "!debug end":
+      print("Powering Off...")
+      await bot.logout()
     if message.content == "!debug test":
-      if message.author.id == "userid":
-        print("Test")
-        await bot.add_reaction(message, '\N{THUMBS UP SIGN}')
-      else:
-        await bot.add_reaction(message, '\N{NO ENTRY}')
-        await bot.send_message(message.channel, "Sorry, Only the owner can use that command.")
-    if message.content == "!debug end":     
-      if message.author.id == "userid":
-        await bot.close()
-        print("Powering off")
-      else:
-        await bot.send_message(message.channel, "You cannot use this command")
-        await bot.add_reaction(message, '\N{NO ENTRY}')
+      print("Test")
+      await bot.add_reaction(message, '\N{THUMBS UP SIGN}')  
     await bot.process_commands(message)
-
+    
 
 
 bot.run(TOKEN)
